1b06b2465d605c0a602584f54fba8c25afbfa690
sayanmondal123 <106104240+sayanmondal123@users.noreply.github.com>
2022-05-23 23:50:52 +0530

    Update guess.php

--- code/arrays/guess.php
+++ code/arrays/guess.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+<title> Sayan Mondal 79fe54d7</title>
 <title>Guessing Game for Charles Severance</title>
 </head>
 <body>
@@ -12,9 +13,9 @@
     echo("Your guess is too short");
   } else if ( ! is_numeric($_GET['guess']) ) {
     echo("Your guess is not a number");
-  } else if ( $_GET['guess'] < 42 ) {
+  } else if ( $_GET['guess'] < 72 ) {
     echo("Your guess is too low");
-  } else if ( $_GET['guess'] > 42 ) {
+  } else if ( $_GET['guess'] > 72 ) {
     echo("Your guess is too high");
   } else {
     echo("Congratulations - You are right");
